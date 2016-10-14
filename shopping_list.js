5db3cb01d214462c02102a3351f27ae8c4277b73
Angelo Pearce <angelopearce85@gmail.com>
2016-10-14 15:45:52 +0200

    assert added

--- shopping_list.js
+++ shopping_list.js
@@ -1,22 +1,19 @@
-
 /*global var*/
-// const assert = require('assert');
+const assert = require('assert');
 
-// var  arrayList = [];
-var shoppingList = function (list) {
+var arrayList = [];
+var shoppingList = function(list) {
 
   var stringSplit = list.split(',');
 
-
-
   for (var i = 0; i < stringSplit.length; i++) {
-  console.log(stringSplit[i]);
-  // arrayList.push(stringSplit[i]);
+    console.log(stringSplit[i]);
+    arrayList.push(stringSplit[i]);
   }
-
-// return arrayList
-//
+  return arrayList
 };
 
-// assert.equal(shoppingList());
-  shoppingList('1x bread,6x hotdog rolls,2x tubs of margarine');
+assert.deepEqual(shoppingList('1x bread,6x hotdog rolls,2x tubs of margarine'), ['1x bread',
+  '6x hotdog rolls',
+  '2x tubs of margarine'
+]);
